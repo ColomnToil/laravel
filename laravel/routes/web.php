2d3dda7d305ea80c8e7d67dc75038672bd3cf9d9
ColomnToil <colomntoil12@mail.ru>
2025-09-10 18:38:33 +0300

    Урок 2

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'aaaaaaaaaaaaaaa';
+});
+
+Route::get('my_page', function(){
+    return 'my page';
 });
